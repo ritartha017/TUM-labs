1214209c899fc574663351f82f591e86d2d843ba
ritartha017 <mirbxa@gmail.com>
2023-05-13 16:49:58 +0300

    update

--- TMPS/LL3/BehavioralPatterns/ChainOfResponsibility/Handler/PaymentHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ChainOfResponsibility.Handler;
-
-abstract class PaymentHandler
-{
-    public PaymentHandler? Successor { get; set; }
-    public abstract void Handle(Receiver receiver);
-}
